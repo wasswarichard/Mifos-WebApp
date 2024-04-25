023680c6bdaec0735bd6f655676358a0050f9fcb
wasswarichard <wasswarichard55@gmail.com>
2024-04-25 17:00:11 +0000

    Deploying to gh-pages from  @ 8ac82cc6a6257eb2e704d263485c238af9c125c5 🚀

--- assets/env.js
+++ assets/env.js
@@ -2,15 +2,34 @@
   window["env"] = window["env"] || {};
 
   // BackEnd Environment variables
-  window["env"]["fineractApiUrls"] = 'https://localhost:8443';
-  window["env"]["fineractApiUrl"]  = 'https://localhost:8443';
+  window["env"]["fineractApiUrls"] = '';
+  window["env"]["fineractApiUrl"]  = '';
 
-  window["env"]["apiProvider"] = '/fineract-provider/api';
-  window["env"]["apiVersion"]  = '/v1';
+  window["env"]["apiProvider"] = '';
+  window["env"]["apiVersion"]  = '';
 
   window["env"]["fineractPlatformTenantId"]  = '';
+  window["env"]["fineractPlatformTenantIds"]  = '';
 
   // Language Environment variables
   window["env"]["defaultLanguage"] = '';
   window["env"]["supportedLanguages"] = '';
+
+  window['env']['preloadClients'] = '';
+
+  // Char delimiter to Export CSV options: ',' ';' '|' ' '
+  window['env']['defaultCharDelimiter'] = '';
+
+  // Display or not the BackEnd Info
+  window['env']['displayBackEndInfo'] = '';
+
+  // Time in seconds for Notifications, default 60 seconds
+  window['env']['waitTimeForNotifications'] = '';
+
+  // Time in seconds for COB Catch-Up, default 30 seconds
+  window['env']['waitTimeForCOBCatchUp'] = '';
+
+  // Time in milliseconds for Session idle timeout, default 300000 seconds
+  window['env']['sessionIdleTimeout'] = '60000';
+
 })(this);
--- assets/env.template.js
+++ assets/env.template.js
@@ -9,8 +9,27 @@
   window["env"]["apiVersion"]  = '$FINERACT_API_VERSION';
 
   window["env"]["fineractPlatformTenantId"]  = '$FINERACT_PLATFORM_TENANT_IDENTIFIER';
+  window["env"]["fineractPlatformTenantIds"]  = '$FINERACT_PLATFORM_TENANTS_IDENTIFIER';
 
   // Language Environment variables
   window["env"]["defaultLanguage"] = '$MIFOS_DEFAULT_LANGUAGE';
   window["env"]["supportedLanguages"] = '$MIFOS_SUPPORTED_LANGUAGES';
+
+  window['env']['preloadClients'] = '$MIFOS_PRELOAD_CLIENTS';
+
+  // Char delimiter to Export CSV options: ',' ';' '|' ' '
+  window['env']['defaultCharDelimiter'] = '$MIFOS_DEFAULT_CHAR_DELIMITER';
+
+  // Display or not the BackEnd Info
+  window['env']['displayBackEndInfo'] = '$MIFOS_DISPLAY_BACKEND_INFO';
+
+  // Time in seconds for Notifications, default 60 seconds
+  window['env']['waitTimeForNotifications'] = '$MIFOS_WAIT_TIME_FOR_NOTIFICATIONS';
+
+  // Time in seconds for COB Catch-Up, default 30 seconds
+  window['env']['waitTimeForCOBCatchUp'] = '$MIFOS_WAIT_TIME_FOR_CATCHUP';
+
+  // Time in milliseconds for Session idle timeout, default 300000 seconds
+  window['env']['sessionIdleTimeout'] = '$MIFOS_SESSION_IDLE_TIMEOUT';
+
 })(this);
